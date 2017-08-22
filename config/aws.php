06b464b7baff05a93b75c99262c52a904458a7b6
JP Caparas <jp@jpcaparas.com>
2017-08-22 23:53:36 +1200

    As Elastic Transcoder config and .env defaults

--- config/aws.php
+++ config/aws.php
@@ -9,7 +9,7 @@ return [
     // S3 service
     's3'                => [
         'version'     => env('AWS_S3_VERSION', 'latest'),
-        'region'      => env('AWS_S3_REGION', 'ap-southeast-2'),
+        'region'      => env('AWS_S3_REGION', 'us-west-1'),
         'bucket_name' => env('AWS_S3_BUCKET_NAME', null),
         'acl'         => env('AWS_S3_ACL', 'public-read'),
         'credentials' => [
@@ -18,9 +18,20 @@ return [
         ],
     ],
 
+    // Elastic Transcoder
+    'et'                => [
+        'version'       => env('AWS_ET_VERSION', 'latest'),
+        'region'        => env('AWS_ET_REGION', 'us-west-1'),
+        'pipeline_id'   => env('AWS_ET_PIPELINE_ID', null),
+        'preset_id'     => env('AWS_ET_PRESET_ID', '1351620000001-000061'), // 360p
+        'bucket_in'     => env('AWS_ET_BUCKET_IN', null),
+        'bucket_out'    => env('AWS_ET_BUCKET_OUT', null),
+        'bucket_thumbs' => env('AWS_ET_BUCKET_THUMBS', null),
+    ],
+
     // Rekognition
     'rekognition'       => [
         'version' => env('AWS_REKOGNITION_VERSION', 'latest'),
-        'region'  => env('AWS_REKOGNITON_REGION', 'us-east-1')
+        'region'  => env('AWS_REKOGNITON_REGION', 'us-west-1')
     ]
 ];
