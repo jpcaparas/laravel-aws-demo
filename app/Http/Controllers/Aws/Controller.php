949f1a5fdbf8dea2a06f0b3ccc3a33710e5450e8
JP Caparas <jp@jpcaparas.com>
2017-08-26 19:52:10 +1200

    Add response convenience functions to base controller

--- app/Http/Controllers/Aws/Controller.php
+++ app/Http/Controllers/Aws/Controller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Aws;
 
 use App\Http\Controllers\Controller as BaseController;
+use Illuminate\Http\Response;
 
 /**
  * Class Controller
@@ -11,5 +12,24 @@ use App\Http\Controllers\Controller as BaseController;
  */
 class Controller extends BaseController
 {
-    //
+    /**
+     * @param array $data
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendErrors(array $data)
+    {
+        return $this->sendResponse(['errors' => $data], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    /**
+     * @param array $data
+     * @param int   $status
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function sendResponse(array $data, $status = 200)
+    {
+        return response()->json($data, $status);
+    }
 }
