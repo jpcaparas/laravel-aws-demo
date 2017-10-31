48ae4fc129691e8f5297246af46422d9ea69a1b7
JP Caparas <jp@jpcaparas.com>
2017-10-31 14:29:17 +1300

    Return response for failed validation

--- app/Http/Requests/Aws/FormRequest.php
+++ app/Http/Requests/Aws/FormRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests\Aws;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest as FormRequestBase;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
 
@@ -27,5 +29,15 @@ class FormRequest extends FormRequestBase
     public function response(array $errors)
     {
         return new JsonResponse(['errors' => $errors], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+            [],
+            JSON_PRETTY_PRINT
+        ));
     }
 }
